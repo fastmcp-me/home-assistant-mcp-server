b844efe904f87db57959bf3ee80aee7904dad17a
Linus Oleander <220827+oleander@users.noreply.github.com>
2025-02-28 22:06:08 +0100

    Delete obsolete light control scripts and add pull-schemas script for maintenance

--- test-light-tools-mock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Make sure TypeScript is compiled
-echo "Building the project..."
-npm run build
-
-# Run the mock light tools test
-echo "Running mock light tools test..."
-node test/test-light-tools-mock.js
--- test-light-tools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Make sure TypeScript is compiled
-echo "Building the project..."
-npm run build
-
-# Run the light tools test
-echo "Running light tools test..."
-node test/test-light-tools.js
